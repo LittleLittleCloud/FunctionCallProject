24d6894721910ba410e07e8f5922cbed31ea8d20
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-10-30 11:51:31 -0700

    init

---
+++ FunctionCallProject/ExampleFunctionCall.cs
@@ -0,0 +1,22 @@
+﻿using AgentChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCallProject
+{
+    public partial class ExampleFunctionCall
+    {
+        /// <summary>
+        /// example function call
+        /// </summary>
+        /// <param name="input">input</param>
+        [FunctionAttribution]
+        public async Task<string> ExampleFunctionCallAsync(string input)
+        {
+            return input;
+        }
+    }
+}
